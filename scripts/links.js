0fd9266c9f007be9b35f8b82779745408af7c09b
woodpecker09 <ortegaharold99@outlook.es>
2025-02-17 22:36:01 -0700

    W05: Team Activity - JSON and the Weather API

--- scripts/links.js
+++ scripts/links.js
@@ -3,12 +3,10 @@ const baseURL = "https://woodpecker09.github.io/wdd230/";
 const linksURL = "https://woodpecker09.github.io/wdd230/data/links.json";
 
 const learnAct = document.querySelector('#weeks'); 
-console.log(learnAct);
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data.weeks);
     displayLinks(data.weeks);
 }
 
@@ -16,16 +14,12 @@ const displayLinks = (weeks) => {
     weeks.forEach(week => {
         const weekLi = document.createElement('li');
         weekLi.textContent = `${week.week}:`;
-        console.log(weekLi);
-        console.log(`${week.week} ${week.links[0].url}`);
         week.links.forEach(link => {
             const info = document.createElement('a');
             info.textContent = ` ${link.title} `;
             info.setAttribute('href', link.url);
-            console.log(info);
             weekLi.appendChild(info);
             weekLi.innerHTML += `|`;
-            console.log(weekLi);
         })
         learnAct.appendChild(weekLi);
     });
