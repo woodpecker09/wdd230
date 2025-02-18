a9129629a9965478edf6eb7ff6b01ffff83a21f9
woodpecker09 <ortegaharold99@outlook.es>
2025-02-17 21:25:20 -0700

    second try

--- scripts/links.js
+++ scripts/links.js
@@ -2,17 +2,33 @@ const baseURL = "https://woodpecker09.github.io/wdd230/";
 
 const linksURL = "https://woodpecker09.github.io/wdd230/data/links.json";
 
+const learnAct = document.querySelector('#weeks'); 
+console.log(learnAct);
+
 async function getLinks() {
     const response = await fetch(linksURL);
-    const data = response.json();
-    console.log(data);
-    // displayLinks(data);
+    const data = await response.json();
+    console.log(data.weeks[0]);
+    displayLinks(data.weeks);
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach(week => {
+        const weekLi = document.createElement('li');
+        weekLi.textContent = `${week.week}:`;
+        console.log(weekLi);
+        console.log(`${week.week} ${week.links[0].url}`);
+        week.links.forEach(link => {
+            const info = document.createElement('a');
+            info.textContent = ` ${link.title} `;
+            info.setAttribute('href', link.url);
+            console.log(info);
+            weekLi.appendChild(info);
+            weekLi.innerHTML += `|`;
+            console.log(weekLi);
+        })
+        learnAct.appendChild(weekLi);
+    });
 }
 
 getLinks();
-
-function displayLinks(weeks){
-    weeks.forEach(week => {
-        console.log(`something in here ${week}`)
-    });
-}
